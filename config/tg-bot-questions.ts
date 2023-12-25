01c379ab87783199b9df372b734d2709395b5d6f
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-12-25 21:28:21 +0200

    feat: add demo messages

--- config/tg-bot-questions.ts
+++ config/tg-bot-questions.ts
@@ -1,3 +1,40 @@
 import { Question } from 'tg-bot/questionnaire';
 
-export const questions: Question[] = [];
+export const questions: Question[] = [
+  {
+    preMessage: {
+      text: 'Hello, I am a questionnaire Telegram bot created by @St_Serge.\n\nPre messages are optional, but with these messages I will guide you through some of the features.',
+    },
+    type: 'string',
+    text: 'What is your name?',
+    responseKey: 'Name',
+  },
+  {
+    preMessage: {
+      text: 'There is no need to provide your personal email, this is just to show you that I can handle email validation.',
+    },
+    type: 'email',
+    text: 'What is your email?',
+    responseKey: 'Email',
+  },
+  {
+    preMessage: {
+      text: 'I can also handle numbers.',
+    },
+    type: 'number',
+    text: 'How old are you?',
+    responseKey: 'Age',
+  },
+  {
+    type: 'boolean',
+    preMessage: {
+      text: 'Did you see one of my other projects?',
+      link: {
+        placeholder: 'Click here:',
+        url: 'https://lookupdomain.biz/',
+      },
+    },
+    text: 'Did you like it?',
+    responseKey: 'Like Previous Project',
+  },
+];
